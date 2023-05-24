bc8170853e152aa817a83aabec87d3792943d35e
Truong Vinh Phuoc <truongphuocst310301@gmail.com>
2023-05-24 19:27:38 +0700

    Update

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/vars/:id')
+  getVarsByRoute(@Param('id') id: string) {
+    return this.appService.getVarsByRoute(id);
+  }
+
+  @Get('/paths/:id/:varId')
+  getPathsByVar(@Param('id') id: string, @Param('varId') varId: string) {
+    return this.appService.getPathsByVars(id, varId);
+  }
+  @Get('/stops/:id/:varId')
+  getStopsByVar(@Param('id') id: string, @Param('varId') varId: string) {
+    return this.appService.getStopsByVars(id, varId);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@nestjs/common';
-
+import { HttpService } from '@nestjs/axios';
+import { map } from 'rxjs';
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private readonly httpService: HttpService) {}
+  getVarsByRoute(id: string) {
+    return this.httpService
+      .get('https://api.xe-buyt.com/businfo/getvarsbyroute/' + id)
+      .pipe(map((res) => res.data));
+  }
+  getPathsByVars(id: string, varId: string) {
+    return this.httpService
+      .get(`https://api.xe-buyt.com/businfo/getpathsbyvar/${id}/${varId}`)
+      .pipe(map((res) => res.data));
+  }
+  getStopsByVars(id: string, varId: string) {
+    return this.httpService
+      .get(`https://api.xe-buyt.com/businfo/getstopsbyvar/${id}/${varId}`)
+      .pipe(map((res) => res.data));
   }
 }
