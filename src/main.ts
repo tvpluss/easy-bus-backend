619b038328b005c264df2ab91e5d44fd7204f494
Truong Vinh Phuoc <truongphuocst310301@gmail.com>
2023-05-28 22:20:36 +0700

    Add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   console.log(process.env.PORT);
+
+  const config = new DocumentBuilder()
+    .setTitle('Bookera Backend')
+    .setDescription(
+      'APIs documentation for backend of bookera, a social book-reading platform',
+    )
+    .setVersion('1.0')
+    .addTag('bookera')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(process.env.PORT);
 }
 bootstrap();
